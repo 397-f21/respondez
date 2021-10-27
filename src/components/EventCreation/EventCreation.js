ead4b9be66d1ebc246639d92b4808a3f3e29a9ba
Jiwon Choi <cjwon531@gmail.com>
2021-10-26 22:22:43 -0500

    small input field UI enhancement

--- src/components/EventCreation/EventCreation.js
+++ src/components/EventCreation/EventCreation.js
@@ -2,60 +2,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // TODO: find the bug and fix it. maybe even set up a test.......
 let changeCapacityLimit = (e, setCapacityLimit) => setCapacityLimit(e.target.checked);
 
-const CapacityForm = (display) => (
-  display ?
-    <form>
-      <div className="form-group">
+const CapacityForm = () => (
+  <>
+    <form className="form-inline">
+      <div className="form-group mb-2">
         <label htmlFor="capacityLimit"> Capacity Limit </label>
         <input className="form-control" id="capacityLimit" type="number" />
       </div>
-      <div className="form-check form-switch">
+    </form>
+    <form>
+      <div className="form-check form-switch mb-4">
         <input className="form-check-input" type="checkbox" id="isThereWaitlist" />
         <label className="form-check-label" htmlFor="isThereWaitlist">Waitlist?</label>
       </div>
     </form>
-    : <></>
+  </>
 )
 
 export const DisplayEventCreator = ({ capacityLimit, setCapacityLimit }) => (
   <>
     <form>
-      <div className="form-group">
-        <label htmlFor="eventName"> Event Name</label>
-        <input type="name" className="form-control" id="eventName" placeholder="Enter event name" />
-      </div>
-      <br />
-      <div id="eventDate" className="md-form md-outline input-with-post-icon datepicker">
-        <label for="eventDate">Event Date</label>
-        <input type="date" id="eventDateInput" class="form-control" />
-      </div>
+      <form className="form-inline mb-4">
+        <div className="form-group">
+          <label htmlFor="eventName" className="me-3"> Event Name</label>
+          <input type="name" className="form-control" id="eventName" placeholder="Enter event name" />
+        </div>
+      </form>
+
+      <form className="form-inline">
+        <div id="eventDate" className="form-group input-with-post-icon datepicker">
+          <label htmlFor="eventDate" className="me-4">Event Date</label>
+          <input type="date" id="eventDateInput" class="form-control" />
+        </div>
+      </form>
       <br />
       <div className="form-group">
         <label htmlFor="eventDescription"> Event Description </label><br />
         <textarea className="form-control" id="eventDescription" rows="3"></textarea>
       </div>
       <br />
-      <div className="form-check form-switch">
+      <div className="form-check form-switch mb-4">
         <input className="form-check-input" type="checkbox" id="isThereCapacity"
           data-target="#capacityCollapse" data-toggle="collapse" />
         <label className="form-check-label" htmlFor="isThereCapacity">Set Capacity Limit?</label>
       </div>
 
       <div id="capacityCollapse" className="collapse">
-        {/* <CapacityForm display={capacityLimit} /> */}
-        <form>
-          <div className="form-group">
-            <label htmlFor="capacityLimit"> Capacity Limit </label>
-            <input className="form-control" id="capacityLimit" type="number" />
-          </div>
-          <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" id="isThereWaitlist" />
-            <label className="form-check-label" htmlFor="isThereWaitlist">Waitlist?</label>
-          </div>
-        </form>
+        <CapacityForm />
       </div>
 
-      <br />
       <div className="form-check">
         <input type="checkbox" className="form-check-input" id="askPhoneNum" />
         <label className="form-check-label" for="askPhoneNum">Ask for Phone Number</label>
