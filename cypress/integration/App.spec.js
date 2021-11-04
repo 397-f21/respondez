fca61d7fedc0a7f843d7c73d2821c77f0a194628
Jiwon Choi <cjwon531@gmail.com>
2021-11-04 16:00:55 -0500

    content cypress test added; test create form button in home page

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,6 +1,10 @@
 describe('Test App', () => {
-
     it('launches', () => {
         cy.visit('/');
+    });
+
+    it('opens with a create form button', () => {
+        cy.visit('/');
+        cy.get('[data-cy=createFormCy]').should('contain', 'Create Form');
     });
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
       <div class="MainScreen">
         <NavLink data-testid="createFormButtonLink" to="/create">
-          <button data-testid="createFormButton" type="button" className="btn btn-primary">
+          <button data-cy="createFormCy" data-testid="createFormButton" type="button" className="btn btn-primary">
             Create Form
           </button>
         </NavLink>
